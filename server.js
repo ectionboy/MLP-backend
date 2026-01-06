e5fc464207200c94596378cd45285f818908c13d
ectionboy <125730248+ectionboy@users.noreply.github.com>
2026-01-06 14:50:21 +0200

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import aiRouter from "./routes/api/ai.js";
-
+import cors from "cors";
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
